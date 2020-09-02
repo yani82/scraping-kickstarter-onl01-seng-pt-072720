38940d455773d4aafba57551eac1fa1c09437560
Yani Asari <yani.iman1@gmail.com>
2020-09-02 05:59:20 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,18 +3,16 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  
-   projects = {}
-   
-   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
-    
-  binding.pry
+  projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+ 
+  # return the projects hash
+  projects
 end
-projects 
-end 
 
 create_project_hash
 project.css("p.bbcard_blurb").text
