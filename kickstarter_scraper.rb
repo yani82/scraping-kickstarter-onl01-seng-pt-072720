7c252736002fdcfd8455666ad220fce18b6a010d
Yani Asari <yani.iman1@gmail.com>
2020-09-02 05:48:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,5 +3,7 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
